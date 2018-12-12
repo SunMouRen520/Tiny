0d6de499f6837b5d5165b97f00fbb58621d0c751
CoreyGao <coreyninggc@gmail.com>
2018-12-12 14:22:23 +0800

    add type definition

---
+++ src/Types.h
@@ -0,0 +1,17 @@
+#ifndef TINY_TYPES_H
+#define TINY_TYPES_H
+
+#include <cstdint>
+
+namespace Tiny{
+	using UnsignedByte = std::uint8_t;
+	using Char = std::int8_t;
+	using UnsignedShort = std::uint16_t;
+	using SHort = std::int16_t;
+	using UnsignedInt = std::uint32_t;
+	using Int = std::int32_t;
+	using UnsignedLong = std::uint64_t;
+	using Long = std::int64_t;
+}
+
+#endif
